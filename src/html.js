ece33ebf34b3536015ee1c8135ffaabe960a9f85
Matias Surdi <matias@surdi.net>
2020-02-29 08:52:34 +0100

    Improve API to avoid having to append the :html suffix everywhere

--- src/html.js
+++ src/html.js
@@ -1,16 +1,29 @@
 import xss from "xss";
 
-
-const toKebabCase = (string) => {
-  return string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+const toKebabCase = string => {
+  return string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1-$2").toLowerCase();
 };
+
+class HtmlString {
+  constructor(value) {
+    this.value = value;
+  }
+
+  render() {
+    return this.value;
+  }
+
+  toString() {
+    return this.value;
+  }
+}
 
 /**
  * A custom template string tag which additionally to do the standard value interpolation
  * it escapes any value which could create XSS vulnerabilities.
  *
- * To skip XSS protection on a given value, append :html after the value interpolation.
- *
+ * To skip XSS protection on a given value, append :safe after the value interpolation. it
+ * is not not necessary to append the :safe modifier if the value is already an html-string.
  *
  * Example:
  *  Given the template:
@@ -22,53 +35,76 @@ const toKebabCase = (string) => {
  * tag will be escaped and thus no XSS will happen.
  *
  * If you need to use unsafe content, thus skip the XSS escaping because you already have manually
- * guaranteed that the value is safe, you can do so by changing the previous template to:
+ * guaranteed that the value is safe and you know what you are doing, then you can do so by changing
+ * the previous template to:
  *
- *    html`This is unsafe ${content}:html`
+ *    html`This is unsafe ${content}:safe`
  *
  * @param {String[]} strings List of strings (split of the literal string at each value)
  * @param  {...any} values List of values to interpolate into the resulting string
  */
-const html = (strings, ...values) =>
+const html = (strings, ...values) => {
   // Reduce the list of strings to a single string
-  strings.reduce((output, currentString, i) => {
+  const resultString = strings.reduce((output, currentString, i) => {
     // Next string to use after the current one and the current value are used, just for looking ahead.
     const nextString = strings[i + 1];
     // Current value to interpolate in the resulting string
-    let currentValue = (values[i] !== null && values[i] !== undefined && values[i] !== false) ? values[i] : "";
+    let currentValue =
+      values[i] !== null && values[i] !== undefined && values[i] !== false
+        ? values[i]
+        : "";
+
+    const isHtmlString = currentValue instanceof HtmlString;
+
+    if (isHtmlString) {
+      currentValue = currentValue.render();
+    }
+
+    // Decide if value should be interpreted as html key=value pairs
+    if (
+      nextString &&
+      nextString.startsWith(":attrs") &&
+      typeof currentValue === "object"
+    ) {
+      currentValue = Object.keys(currentValue)
+        .map(key => {
+          if (typeof currentValue[key] === "boolean") {
+            if (currentValue[key] === false) {
+              return "";
+            }
+            return `${toKebabCase(key)}`;
+          }
+          return `${toKebabCase(key)}="${currentValue[key]}"`;
+        })
+        .filter(Boolean)
+        .join(" ");
+    }
+
+    // Decide if XSS protection is necessary, by looking if the next line starts by :safe
+    if (!nextString || !nextString.startsWith(":safe")) {
+      if (Array.isArray(currentValue)) {
+        currentValue.map(v =>
+          v instanceof HtmlString ? v : xss(v.toString())
+        );
+      } else {
+        currentValue = xss(currentValue.toString());
+      }
+    }
 
     // If current value is an array, then transform it to a string by joining its values with an empty space.
     if (Array.isArray(currentValue)) {
       currentValue = currentValue.join(" ");
     }
 
-
-    if (nextString !== undefined) {
-      // Decide if value should be interpreted as html key=value pairs
-      if (nextString.startsWith(':attrs') && typeof currentValue === 'object') {
-        currentValue = Object.keys(currentValue).map(key => {
-          if (typeof currentValue[key] === 'boolean') {
-            if (currentValue[key] === false) {
-              return ""
-            }
-            return `${toKebabCase(key)}`
-          }
-          return `${toKebabCase(key)}="${currentValue[key]}"`
-        }).filter(Boolean).join(" ");
-      }
-
-      // Decide if XSS protection is necessary, by looking if the next line starts by :html
-      else if (!nextString.startsWith(":html")) {
-        currentValue = xss(currentValue.toString());
-      }
-    }
-
-    // Clean any existing ":html" string from current string
-    const cleanCurrentString = currentString.replace(/:(html|attrs)/g, "");
+    // Clean any existing ":<modifier>" string from output
+    const cleanCurrentString = currentString.replace(/:(safe|attrs)/g, "");
 
     // Return resulting string up to this point
     return `${output}${cleanCurrentString}${currentValue}`;
   }, "");
+
+  return new HtmlString(resultString);
+};
 
 export default html;
 module.exports = html;
--- src/html.test.js
+++ src/html.test.js
@@ -3,73 +3,90 @@ const html = require("./html");
 describe("Html templates", () => {
   it("Renders a basic template", () => {
     const template = () =>
-      html `
+      html`
         <p>A simple template</p>
       `;
 
-    expect(template().trim()).toEqual("<p>A simple template</p>");
+    expect(
+      template()
+        .render()
+        .trim()
+    ).toEqual("<p>A simple template</p>");
   });
 
   it("Interpolates simple values", () => {
     const template = value =>
-      html `
+      html`
         <p>A simple template with a value of ${value}</p>
       `;
 
-    expect(template("funky").trim()).toEqual(
-      "<p>A simple template with a value of funky</p>"
-    );
+    expect(
+      template("funky")
+        .render()
+        .trim()
+    ).toEqual("<p>A simple template with a value of funky</p>");
   });
 
   it("Renders a falsy value as an empty string except if it is a number", () => {
     const template = value =>
-      html `
+      html`
         <p>A simple template with a value of ${value}</p>
       `;
 
-    expect(template(false).trim()).toEqual(
-      "<p>A simple template with a value of </p>"
-    );
+    expect(
+      template(false)
+        .render()
+        .trim()
+    ).toEqual("<p>A simple template with a value of </p>");
 
-    expect(template("").trim()).toEqual(
-      "<p>A simple template with a value of </p>"
-    );
+    expect(
+      template("")
+        .render()
+        .trim()
+    ).toEqual("<p>A simple template with a value of </p>");
 
-    expect(template(null).trim()).toEqual(
-      "<p>A simple template with a value of </p>"
-    );
+    expect(
+      template(null)
+        .render()
+        .trim()
+    ).toEqual("<p>A simple template with a value of </p>");
 
-    expect(template(0).trim()).toEqual(
-      "<p>A simple template with a value of 0</p>"
-    );
-
+    expect(
+      template(0)
+        .render()
+        .trim()
+    ).toEqual("<p>A simple template with a value of 0</p>");
   });
 
   it("Interpolates an array as a string", () => {
     const template = value =>
-      html `
+      html`
         <p>A simple template with a value of ${value}</p>
       `;
 
-    expect(template(["funky", "funny"]).trim()).toEqual(
-      "<p>A simple template with a value of funky funny</p>"
-    );
+    expect(
+      template(["funky", "funny"])
+        .render()
+        .trim()
+    ).toEqual("<p>A simple template with a value of funky funny</p>");
   });
 
   it("Interpolates an undefined value as an empty string", () => {
     const template = value =>
-      html `
+      html`
         <p>A simple template with a value of ${value}.</p>
       `;
 
-    expect(template(undefined).trim()).toEqual(
-      "<p>A simple template with a value of .</p>"
-    );
+    expect(
+      template(undefined)
+        .render()
+        .trim()
+    ).toEqual("<p>A simple template with a value of .</p>");
   });
 
   it("Interpolates an object value as its toString() value", () => {
     const template = value =>
-      html `
+      html`
         <p>A simple template with a value of ${value}.</p>
       `;
     const testValue = {
@@ -77,74 +94,121 @@ describe("Html templates", () => {
         return "Serialized";
       }
     };
-    expect(template(testValue).trim()).toEqual(
-      "<p>A simple template with a value of Serialized.</p>"
-    );
+    expect(
+      template(testValue)
+        .render()
+        .trim()
+    ).toEqual("<p>A simple template with a value of Serialized.</p>");
   });
 
   it("Escapes dangerous values (xss)", () => {
     const template = value =>
-      html `
+      html`
         <p>A simple template with a value of ${value}</p>
       `;
 
-    expect(template("<script>alert('boom!')</script>").trim()).toEqual(
+    expect(
+      template("<script>alert('boom!')</script>")
+        .render()
+        .trim()
+    ).toEqual(
       "<p>A simple template with a value of &lt;script&gt;alert('boom!')&lt;/script&gt;</p>"
     );
   });
 
-  it("Does not escape dangerous values with the :html modifier", () => {
+  it("Does not escape templates passed as values", () => {
+    const templateValue = () =>
+      // prettier-ignore
+      html`<h1>A value with tags</h1>`;
+
+    // prettier-ignore
+    const template = value =>html`<div>${value}</div>`;
+
+    expect(
+      template(templateValue())
+        .render()
+        .trim()
+    ).toEqual(`<div><h1>A value with tags</h1></div>`);
+  });
+
+  it("Does not escape arrays of templates passed as values", () => {
+    // prettier-ignore
+    const templateValue = v =>
+      html`<li>A value ${v} with tags</li>`;
+
+    // prettier-ignore
+    const template = values =>html`<ul>${values.map(v => templateValue(v))}</ul>`;
+
+    expect(
+      template(["a", "b", "c"])
+        .render()
+        .trim()
+    ).toEqual(
+      "<ul><li>A value a with tags</li> <li>A value b with tags</li> <li>A value c with tags</li></ul>"
+    );
+  });
+
+  it("Does not escape dangerous values with the :safe modifier", () => {
     const template = value =>
-      html `
-        <p>A simple template with a value of ${value}:html</p>
+      html`
+        <p>A simple template with a value of ${value}:safe</p>
       `;
 
-    expect(template("<script>alert('boom!')</script>").trim()).toEqual(
+    expect(
+      template("<script>alert('boom!')</script>")
+        .render()
+        .trim()
+    ).toEqual(
       "<p>A simple template with a value of <script>alert('boom!')</script></p>"
     );
   });
 
   it("Transforms any object value with a :attrs suffix to html attributes", () => {
     const template = value =>
-      html `
-      <p ${value}:attrs></p>
-    `;
+      html`
+        <p ${value}:attrs></p>
+      `;
 
-    expect(template({
-      id: 1,
-      name: "value",
-      dataCustom: "customData"
-    }).trim()).toEqual(
-      '<p id="1" name="value" data-custom="customData"></p>'
-    );
-  })
+    expect(
+      template({
+        id: 1,
+        name: "value",
+        dataCustom: "customData"
+      })
+        .render()
+        .trim()
+    ).toEqual('<p id="1" name="value" data-custom="customData"></p>');
+  });
 
   it(":attrs modifier preserves boolean attribute without a value", () => {
     const template = value =>
-      html `
-      <p ${value}:attrs></p>
-    `;
+      html`
+        <p ${value}:attrs></p>
+      `;
 
-    expect(template({
-      id: 1,
-      disabled: true
-    }).trim()).toEqual(
-      '<p id="1" disabled></p>'
-    );
-  })
+    expect(
+      template({
+        id: 1,
+        disabled: true
+      })
+        .render()
+        .trim()
+    ).toEqual('<p id="1" disabled></p>');
+  });
 
   it(":attrs modifier removes boolean attribute", () => {
     const template = value =>
-      html `
-      <p ${value}:attrs></p>
-    `;
+      html`
+        <p ${value}:attrs></p>
+      `;
 
-    expect(template({
-      id: 1,
-      disabled: false
-    }).trim()).toEqual(
-      '<p id="1"></p>'
-    );
-  })
-
+    expect(
+      template({
+        id: 1,
+        disabled: false
+      })
+        .render()
+        .trim()
+    ).toEqual('<p id="1"></p>');
+  });
 });
