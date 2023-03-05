cc30ee1b4065e859194bd9ed3698a55b209c76c4
Matías Surdi <matiassurdi@gmail.com>
2023-03-05 17:43:41 +0100

    Remove outdated babel dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,12 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
-    jest: true
+    jest: true,
   },
   extends: ["airbnb-base", "prettier"],
   plugins: ["prettier"],
-  parser: "babel-eslint",
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2018,
   },
-  rules: {}
+  rules: {},
 };
